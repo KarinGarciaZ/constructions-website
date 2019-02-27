54270a681fd1c7fdba3edaf4ede1db234f49f8f4
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-26 18:06:07 -0800

    File updated.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from './hoc/Layout';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
     );
   }
 }
