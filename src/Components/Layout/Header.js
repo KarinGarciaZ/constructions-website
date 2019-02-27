0ac399d8fbd0d489584001cf63d27a784edba0fb
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-27 09:49:38 -0800

    Header working for all casses.

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from '../../Assets/Images/logo.png';
 
 class Header extends Component {
   render() {
+
+    let classes = ['header']
+    if( this.props.classes )
+      classes.push(this.props.classes)
+
     return (
-      <div className='header'>
-        header
+      <div className={classes.join(' ')}>
+        <div className='company'>
+          <img className='company__logo' src={logo} alt='img_logo'/>
+          <span className='company__name'>company name</span>
+        </div>
+        <NavLink to='/all-constructions' activeClassName='nav-active' className='nav' >
+          Our Constructions
+        </NavLink>
+        <NavLink to='/' activeClassName='nav-active' className='nav' >
+          Our Services
+        </NavLink>
+        <NavLink to='/about-us' activeClassName='nav-active' className='nav' >
+          About Us
+        </NavLink>
       </div>
     )
   }
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import Header from '../Components/Layout/Header';
 
 class Home extends Component {
   render() {
     return (
       <div className='home'>
-        home
+        <Header classes='transparent' />
+        <div className='image-container'>
+
+        </div>
+        <div className='services'>
+          
+        </div>
+        <div className='about-company'>
+
+        </div>
+        <div className='carrucel'>
+
+        </div>
+        <div className='contact-us'>
+
+        </div>
       </div>
     )
   }
