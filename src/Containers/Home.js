77bb828a10086d89f3d41842a825c2b165d3e9bf
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-27 19:47:55 -0800

    Added some form styles.

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -24,7 +24,20 @@ class Home extends Component {
 
         </div>
         <div className='contact-us'>
-          <div></div>
+          <div className='contact-us__img'>
+            <div className='contact-us__img--form'>
+
+              <h2 className='form-title'>
+                Send us an e-mail!
+              </h2>
+
+              <div className='form-group'>
+                <input type='email' className='input' placeholder='Email' id='email' required />
+                <label className='label' htmlFor='email'>Email</label>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
     )
