3cc79e77aba34e8cd2a5fe8eef5d21a829ee937a
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-03-01 11:27:23 -0800

    Services cards made dynamic.

--- src/Components/Cards/ServiceCard.js
+++ src/Components/Cards/ServiceCard.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faToolbox } from "@fortawesome/free-solid-svg-icons";
 
-const ServiceCard = () => {
+const ServiceCard = (props) => {
   return (
     <div className='service-card'>
-      <FontAwesomeIcon icon={faToolbox} className='service-card__icon'/>
-      <p className='service-card__title'>Service Title</p>
+      {props.icon}
+      <p className='service-card__title'>{props.title}</p>
       <p className='service-card__description'>
-        El Lorem Ipsum fue concebido como un texto de relleno, 
-        formateado de una cierta manera para permitir la presentación
-        de elementos gráficos en documentos, sin necesidad de una copia formal.
+        {props.children}
       </p>
     </div>
   )
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faToolbox, faUserCog, faHandHoldingUsd, faLock } from "@fortawesome/free-solid-svg-icons";
 
 import Header from '../Components/Layout/Header';
 import ServiceCard from '../Components/Cards/ServiceCard';
 import EmailSection from '../Components/Sections/Form';
+import AboutCompanySection from '../Components/Sections/AboutCompany';
 import CarruselConstructions from '../Components/Sections/CarruselConstructions';
+
 
 class Home extends Component { 
 
@@ -30,22 +34,30 @@ class Home extends Component {
   }
 
   render() {
+
+    let icon1 = <FontAwesomeIcon icon={faToolbox} className='service-card__icon'/>
+    let icon2 = <FontAwesomeIcon icon={faUserCog} className='service-card__icon'/>
+    let icon3 = <FontAwesomeIcon icon={faHandHoldingUsd} className='service-card__icon'/>
+    let icon4 = <FontAwesomeIcon icon={faLock} className='service-card__icon'/>
+
     return (
       <div className='home'>
         <Header classes={this.state.colorHeader} />
-        <div className='image-container'>
 
-        </div>
+        <div className='image-container'></div>
+
         <div className='services'>
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
+          <h2 className='services__title u-title-2'>We are your best choice!</h2>
+          <ServiceCard title='Best Service' icon={icon1}>We use high technology tools and machines to give a better, safe and faster service to our clients.</ServiceCard>
+          <ServiceCard title='Best Service' icon={icon2}>We offer a high quality service with a profesional personal.</ServiceCard>
+          <ServiceCard title='Best Service' icon={icon3}>We have the best price in market.</ServiceCard>
+          <ServiceCard title='Best Service' icon={icon4}>Constructions 100% secure, we are a trustable company.</ServiceCard>
         </div>
-        <div className='about-company'>
 
-        </div>
+        <AboutCompanySection />
+
         <CarruselConstructions { ...this.props } />
+
         <EmailSection />
       </div>
     )
