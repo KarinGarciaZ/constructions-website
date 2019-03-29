c522a6621ca2191ca100a9daa6e9c17027c1db8c
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-03-28 19:03:55 -0700

    Created scss component to no data.

--- src/Components/NoData.js
+++ src/Components/NoData.js
@@ -4,11 +4,9 @@ import img404 from '../Assets/Images/404.jpg';
 
 const NoData = (props) => {
   return (
-    <div className='constructions__container'>
-      <div className='constructions__container--none'>
-        <img src={img404} alt='loading-icon' className='img404'/>
-        <p>We couldn't find {props.item} with these characteristics.</p>
-      </div>
+    <div className='no-data'>
+      <img src={img404} alt='loading-icon' className='img404'/>
+      <p>We couldn't find {props.item}</p>
     </div>
   )
 }
